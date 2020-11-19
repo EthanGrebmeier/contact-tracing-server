f1be559cbe0816a46bbb92475cfb8a68d6156db0
Ethan Grebmeier <ethangrebmeier@gmail.com>
2020-11-18 20:58:05 -0800

    Added registration rout

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const http = require('http'); 
 const express = require('express'); 
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express() // setup express application 
+const passport = require('passport')
+const passportLocal = require('passport-local').Strategy
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express(); 
 
 const port = process.env.PORT || 5000
 
-app.use(cors())
 
 // Parse incoming requests data 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
+
+app.use('/api/authenticate', require('./routes/api/authenticate'))
 
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/sessions', require('./routes/api/sessions'))
-
 
 const server = app.listen(port, () => {
     console.log(`Server running at Port: ${port}`);
---
+++ routes/api/authenticate.js
@@ -0,0 +1,133 @@
+const express = require('express')
+const router = express.Router()
+
+const bcrypt = require('bcryptjs')
+
+const db = require('../../pgp');
+
+router.post('/register', (req, res) => {
+  console.log(req.body)
+  db.task('register-user', async t => {
+
+    let user = await db.any(`
+      SELECT * FROM USERS 
+      WHERE email = $1
+    `, [req.body.email])
+
+    console.log(user)
+
+    if (!checkSignUpForm(req.body.firstName, req.body.lastName, req.body.email, req.body.password)){
+      res.status(400).send("Invalid Request")
+    } else if(user.length != 0){
+      res.status(400).send("Email in use")
+    } else {
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, async function(err, hash){
+
+          if (err) console.log(err)
+
+          let fullName = `${req.body.firstName} ${req.body.lastName}`
+
+          let friendCode = Math.floor(Math.random() * 1000000000).toString();
+          console.log(`Friend Code: ${friendCode}`)
+          let checkID = await db.any(`
+            SELECT * FROM USERS WHERE code = $1
+          `, [friendCode])
+
+          while (checkID.length != 0){
+            let friendCode = Math.floor(Math.random() * 1000000000).toString();
+            let checkID = await db.any(`
+              SELECT * FROM USERS WHERE code = $1
+            `, [friendCode])
+          }
+    
+          let createUser = await db.any(`
+            INSERT INTO USERS 
+            (name, code, created_at, status, email, password)
+            VALUES ($1, $2, clock_timestamp(), 'healthy', $3, $4)
+          `, [fullName, friendCode, req.body.email, hash])
+
+          let newUser = await db.any(`
+            SELECT id FROM users WHERE code = $1
+          `, [friendCode])
+
+          console.log(newUser)
+
+          res.json({
+            id: newUser[0]
+          })
+        })
+      })
+    }
+  })
+})
+
+var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+function isEmailValid(email) {
+  console.log(email)
+    if (!email)
+        return false;
+
+    if(email.length>254)
+        return false;
+
+    var valid = emailRegex.test(email);
+    if(!valid)
+        return false;
+
+    // Further checking of some things regex can't handle
+    var parts = email.split("@");
+    if(parts[0].length>64)
+        return false;
+
+    var domainParts = parts[1].split(".");
+    if(domainParts.some(function(part) { return part.length>63; }))
+        return false;
+
+    return true;
+}
+
+function checkForCapital(password){
+  for (let i = 0; i < password.length; i++){
+      let charCode = password.charCodeAt(i)
+      if (charCode > 64 && charCode < 90){
+          return true
+      }
+  }
+  return false
+}
+
+function checkForNumber(password){
+  for (let i = 0; i < password.length; i++){
+      let charCode = password.charCodeAt(i)
+      if (charCode > 47 && charCode < 58){
+          return true
+      }
+  }
+  return false
+}
+
+function checkSignUpForm(firstName, lastName, email, password, confirmPassword){
+  event.preventDefault()
+  if (firstName === "" ){
+    return false
+  } else if (lastName === ""){
+    return false
+  } else if (email === ""){
+    return false
+  } else if (password === ""){
+    return false
+  } else if (!isEmailValid(email)){
+    return false
+  } else if (password.length < 7){
+    return false
+  } else if (!checkForCapital(password)){
+    return false
+  } else if (!checkForNumber(password)){
+    return false
+  } 
+  return true
+}
+
+module.exports = router;
