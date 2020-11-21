e7622481357b4f6c447ba97792590ddf18998fc0
Ethan Grebmeier <ethangrebmeier@gmail.com>
2020-11-20 16:36:23 -0800

    Removed some logging

--- authJWT.js
+++ authJWT.js
@@ -11,9 +11,6 @@ verifyToken = async (req, res, next) => {
     SELECT * from users where id = $1
   `, id)
 
-  console.log("USER: ")
-  console.log(user)
-
   if (!token) {
     return res.status(403).send({
       message: "No token provided!"
@@ -21,15 +18,12 @@ verifyToken = async (req, res, next) => {
   }
 
   jwt.verify(token, process.env.TOKENSECRET, (err, decoded) => {
-    console.log("DECODED")
-    console.log(decoded)
     if (err || decoded["userID"] != id) {
       return res.status(401).send({
         message: "Unauthorized!"
       });
     }
     req.userID = decoded.userID;
-    console.log(req)
     next();
   });
 };
